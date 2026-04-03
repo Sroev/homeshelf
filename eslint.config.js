ba11bda7a905a54e960ffbc9a20aca3196e79feb
Sroev <stoyan.roev@gmail.com>
2026-04-03 13:00:52 +0300

    Add rule to warn on unused variables

--- eslint.config.js
+++ eslint.config.js
@@ -26,6 +26,7 @@ export default tseslint.config(
       "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/no-empty-object-type": "warn",
       "@typescript-eslint/no-require-imports": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",  // ← add this line
     },
   }
 );
